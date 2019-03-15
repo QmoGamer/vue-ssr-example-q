f853a8d2e99b22c436f3fffa0de2eb46fb354c05
Qmo <qmo.chiu@baifu-tech.net>
2019-03-15 11:00:10 +0800

    step2

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const Vue = require('vue')
-const app = new Vue({
-  template: `<div>Hello World</div>`
-})
-
+const Koa = require('koa')
+const Router = require('koa-router')
 const renderer = require('vue-server-renderer').createRenderer()
 
-renderer.renderToString(app, (err, html) => {
-  if (err) throw err
-  console.log(html)
+const koaApp = new Koa()
+const koaRouter = new Router()
+
+koaRouter.get('*', ctx => {
+  const app = new Vue({
+    data: {
+      url: ctx.url
+    },
+    template: `<div>當前 URL 是： {{ url }}</div>`
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      ctx.status = 500
+      ctx.body = 'Internal Server Error'
+      throw err
+    }
+    ctx.status = 200
+    ctx.body = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `
+  })
+})
+
+koaApp
+  .use(koaRouter.routes())
+  .use(koaRouter.allowedMethods())
+
+koaApp.listen(3100, () => {
+  console.log(`server started at localhost:3100`)
 })
