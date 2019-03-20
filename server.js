db61df455248f84c8811dab89ae198492f8c2159
Qmo <qmo.chiu@baifu-tech.net>
2019-03-20 15:04:48 +0800

    Demo v1

--- server.js
+++ server.js
@@ -1,45 +1,91 @@
-const Vue = require('vue')
-const Koa = require('koa')
-const Router = require('koa-router')
-const fs = require('fs')
-const renderer = require('vue-server-renderer').createRenderer({
-  template: fs.readFileSync('./index.template.html', 'utf-8')
-})
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { createBundleRenderer } = require('vue-server-renderer');
+const devServer = require('./build/setup-dev-server')
+const resolve = file => path.resolve(__dirname, file);
 
-const koaApp = new Koa()
-const koaRouter = new Router()
+const isProd = process.env.NODE_ENV === 'production';
+const app = express();
 
-koaRouter.get('*', ctx => {
-  const app = new Vue({
-    data: {
-      url: ctx.url
-    },
-    template: `<div>當前 URL 是： {{ url }}</div>`
-  })
+const serve = (path, cache) =>
+  express.static(resolve(path), {
+    maxAge: cache && isProd ? 1000 * 60 * 60 * 24 * 30 : 0
+  });
+app.use('/dist', serve('./dist', true));
+
+function createRenderer(bundle, options) {
+  return createBundleRenderer(
+    bundle,
+    Object.assign(options, {
+      basedir: resolve('./dist'),
+      runInNewContext: false
+    })
+  );
+}
+
+function render(req, res) {
+  const startTime = Date.now();
+  res.setHeader('Content-Type', 'text/html');
+
+  const handleError = err => {
+    if (err.url) {
+      res.redirect(err.url);
+    } else if (err.code === 404) {
+      res.status(404).send('404 | Page Not Found');
+    } else {
+      res.status(500).send('500 | Internal Server Error');
+      console.log(err)
+    }
+  };
 
   const context = {
-    title: ctx.url,
-    meta: `
-      <meta charset="UTF-8">
-      <meta name="descript" content="基于webpack、koa搭建的SSR">
-    `
-  }
-
-  renderer.renderToString(app, context, (err, html) => {
+    title: 'SSR 测试', // default title
+    url: req.url
+  };
+  renderer.renderToString(context, (err, html) => {
     if (err) {
-      ctx.status = 500
-      ctx.body = 'Internal Server Error'
-      throw err
+      return handleError(err);
     }
-    ctx.status = 200
-    ctx.body = html
-  })
-})
+    res.send(html);
+    if (!isProd) {
+      console.log(`whole request: ${Date.now() - startTime}ms`);
+    }
+  });
+}
 
-koaApp
-  .use(koaRouter.routes())
-  .use(koaRouter.allowedMethods())
+let renderer;
+let readyPromise;
+const templatePath = resolve('./src/index.template.html');
 
-koaApp.listen(3100, () => {
-  console.log(`server started at localhost:3100`)
-})
+if (isProd) {
+  const template = fs.readFileSync(templatePath, 'utf-8');
+  const bundle = require('./dist/vue-ssr-server-bundle.json');
+  const clientManifest = require('./dist/vue-ssr-client-manifest.json') // 将js文件注入到页面中
+  renderer = createRenderer(bundle, {
+    template,
+    clientManifest
+  });
+} else {
+  readyPromise = devServer(
+    app,
+    templatePath,
+    (bundle, options) => {
+      renderer = createRenderer(bundle, options);
+    }
+  );
+}
+
+app.get(
+  '*',
+  isProd
+    ? render
+    : (req, res) => {
+      readyPromise.then(() => render(req, res));
+    }
+);
+
+const port = process.env.PORT || 3100;
+app.listen(port, () => {
+  console.log(`server started at localhost:${port}`);
+});
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePage from './views/HomePage.vue'
+
+Vue.use(Router)
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    routes: [
+      { path: '/', component: HomePage }
+    ]
+  })
+} 
